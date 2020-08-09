260546ce96d678b36024c60f994ed46612fdcb37
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-09 02:07:56 +0100

    setting up the application - adding Major Importations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import Routes from './routes'
 
+import reducers from './store/reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={ createStoreWithMiddleware(reducers) }>
+      <BrowserRouter>
+        <Routes/>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
