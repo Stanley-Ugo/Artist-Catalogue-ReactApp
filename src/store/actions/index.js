7891df4723f4467967524bedd33180800006bd54
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-10 05:52:07 +0100

    adding an artistListAll action(function) to index.js action

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,1 +1,17 @@
+import {
+    GET_ARTIST_ALL
+} from '../types';
 
+import axios from 'axios';
+const URL = 'http://localhost:3004';
+
+export function artistListAll(){
+
+    const request = axios.get(`${URL}/artists`)
+                    .then( response => response.data );
+
+    return {
+        type: GET_ARTIST_ALL,
+        payload: request
+    }
+}
