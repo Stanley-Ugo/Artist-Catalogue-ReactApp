82d4025e0abbc71c9f681902c4278c1e0e1daee6
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-08-10 22:55:00 +0100

    adding a mapStateToprops & componentDidMount to Artist.js component

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,6 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { artistDetail } from '../store/actions';
+import { Link } from 'react-router-dom';
+
 
 class Artist extends Component {
+
+    componentDidMount(){
+        this.props.dispatch(artistDetail(this.props.match.params.id));
+    }
 
     render() {
         return(
@@ -11,4 +19,10 @@ class Artist extends Component {
     }
 }
 
-export default Artist;
+function mapStateToProps(state){
+    return {
+        artists: state.artists
+    }
+}
+
+export default connect(mapStateToProps)(Artist);
